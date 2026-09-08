64d904b92538408aff032316724defa9057db6f8
Guanran Wang <guanran928@outlook.com>
2026-09-08 15:45:56 +0800

    fix(about_page): add spacing between chinese & latin

--- lib/page/about/about_page.dart
+++ lib/page/about/about_page.dart
@@ -347,7 +347,7 @@ class _AboutPageState extends State<AboutPage> {
             ListTile(
               leading: Icon(Icons.favorite),
               title: Text(I18n.of(context).thanks),
-              subtitle: Text('感谢帮助我测试的弹幕委员会群友们\n感谢pixiv cat站主提供的图床'),
+              subtitle: Text('感谢帮助我测试的弹幕委员会群友们\n感谢 pixiv cat 站主提供的图床'),
               onTap: () {
                 if (Platform.isAndroid)
                   Navigator.of(context).push(
